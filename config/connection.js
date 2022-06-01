c6033f6a55e87837dddaa1745583b971787c0bd8
joel <joelthobias447@gmail.com>
2022-06-01 10:14:18 +0530

    mongodb cluster url added

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,8 @@ const state={
     db:null
 }
 module.exports.connect=(done)=>{
-    const url ='mongodb://localhost:27017'
+//     const url ='mongodb://localhost:27017'
+    const url='mongodb+srv://joel:Qwertyuiop@1@cluster0.chsu5.mongodb.net/admin'
     const dbname='todo'
     
     MongoClient.connect(url,(err,data)=>{
